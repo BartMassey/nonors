1e349522bec523010bb107038f6e839ae9e9ebc2
Bart Massey <bart.massey@gmail.com>
2021-08-16 23:32:57 -0700

    moved to proper argument parsing, added --all

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 mod board;
 mod clues;
 
+use std::path::PathBuf;
+use structopt::StructOpt;
+
 use board::*;
 use clues::*;
 
-fn solve(clues: &Clues, board: &mut Board, r: usize, c: usize) {
+fn solve_brute(
+    clues: &Clues,
+    board: &mut Board,
+    r: usize,
+    c: usize,
+    find_all: bool,
+) -> bool {
     let (nr, nc) = clues.dims();
     if r >= nr {
         if board.solved(clues) {
             println!("{}", board);
+            return true;
         }
-        return;
+        return false;
     }
 
     let (mut next_r, mut next_c) = (r, c);
@@ -21,17 +31,31 @@ fn solve(clues: &Clues, board: &mut Board, r: usize, c: usize) {
     }
 
     board.set(r, c, false);
-    solve(clues, board, next_r, next_c);
+    let solved = solve_brute(clues, board, next_r, next_c, find_all);
+    if !find_all && solved {
+        return true;
+    }
     board.set(r, c, true);
-    solve(clues, board, next_r, next_c);
+    let solved = solved | solve_brute(clues, board, next_r, next_c, find_all);
+    solved
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "nonors", about = "Nonogram solver.")]
+struct Opt {
+    #[structopt(short = "a", long = "all")]
+    find_all: bool,
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
 }
 
 fn main() {
-    let path = std::env::args().nth(1).unwrap();
-    let desc = std::fs::read_to_string(path).unwrap();
+    let opt = Opt::from_args();
+
+    let desc = std::fs::read_to_string(opt.path).unwrap();
     let clues = Clues::parse(&desc).unwrap();
     let (nrows, ncols) = clues.dims();
     let mut board = Board::new(nrows, ncols);
-
-    solve(&clues, &mut board, 0, 0);
+    let solved = solve_brute(&clues, &mut board, 0, 0, opt.find_all);
+    std::process::exit((!solved) as i32);
 }
